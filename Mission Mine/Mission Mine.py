33f99fce35e9c4eec3ef66c2ad9f339172b5a89a
David Tibbits <65333750+dmt2172@users.noreply.github.com>
2021-12-19 23:43:11 -0500

    Add files via upload

---
+++ Mission Mine/Mission Mine.py
@@ -0,0 +1,237 @@
+"""
+Final Project
+EAEE 4200
+Mission Mine Flow Sheet
+
+Tkinter and Python
+
+Henry Manelski and David Tibbits
+December 2021
+"""
+
+#Importation of important modules (Directly from MinLab0.0.2)
+from tkinter import *
+from PIL import Image, ImageTk
+from tkinter import filedialog
+from pathlib import Path
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import sys 
+import random
+import os
+import glob
+
+#Get Current directory, set as cd.
+cd = os.getcwd()
+
+#establish root for tkinter
+root = Tk()
+root.geometry("1440x900")
+root.title('Mission Mine Flow Sheet V.1.0')
+
+#Creates background image
+img = ImageTk.PhotoImage(Image.open(cd + "/Background.png"))
+background = Label(root, image = img)
+background.place(relx=0.5, rely=0.5, anchor='center')  
+
+#creates foreground frame
+foreground = Frame(root).place(x=0, y=0)
+
+#creates taskbar frame on sie of foreground
+Taskbar = Frame(foreground).place(x=1100, y=0)
+
+#functions
+def update():
+    pass
+
+def Cycl1():
+    global cy1_sheet
+    global df_1
+    global P50_1
+    global P80_1
+    global graph1_label
+    
+    cy1_sheet = filedialog.askopenfilename(title="Open a Spreadsheet", initialdir=cd)
+    
+    df_1 = pd.read_csv(cy1_sheet)
+
+    df_1['V(x)'] = (df_1['F(x)']*0.4/5.6)/0.678
+    df_1['Cumulative V(x)'] = np.cumsum(df_1['V(x)'])
+
+    P50_1 = float(df_1.iloc[(np.cumsum(df_1['F(x)'])-0.5).abs().argsort()[:1]]['x'])
+    P80_1 = float(df_1.iloc[(np.cumsum(df_1['F(x)'])-0.8).abs().argsort()[:1]]['x'])
+    
+    F_80 = 150000
+    P_80 = np.linspace(10000,500,500)
+
+    W_i = 15
+    E_G = 10*W_i*((1/np.sqrt(P_80)-(1/np.sqrt(F_80))))
+
+    plt.plot(P_80,E_G,label=r'$15$ (kWh/t)')
+
+    W_i = 20
+    E_G = 10*W_i*((1/np.sqrt(P_80)-(1/np.sqrt(F_80))))
+
+    plt.plot(P_80,E_G,label=r'$20$ (kWh/t)')
+
+    W_i = 25
+    E_G = 10*W_i*((1/np.sqrt(P_80)-(1/np.sqrt(F_80))))
+
+    plt.plot(P_80,E_G,label=r'$25$ (kWh/t)')
+    plt.title(r'$E_G$ vs $P_{80}$ (SAG Mill, $F_{80}=1.5$ cm)')
+
+    plt.legend(title=r'$W_i$')
+    plt.xlabel(r'$P_{80}$ (microns)') 
+    plt.ylabel(r'$E_G$ (kWh/t)')
+    plt.savefig('1.jpg',dpi=500)
+
+
+    image = Image.open(cd + "/2.jpg")
+    image.resize(300,200)
+    
+    g1 = ImageTk.PhotoImage(Image.open(image))
+    graph1_label = Label(foreground, image=g1).place(x=1100, y=400)
+    
+    pass
+
+def Cycl2():
+    global cy2_sheet
+    global df_2
+    global P50_2
+    global P80_2
+    global wi_var
+    
+    cy2_sheet = filedialog.askopenfilename(title="Open a Spreadsheet", initialdir=cd)
+    
+    df_2 = pd.read_csv(cy1_sheet)
+
+    df_2['V(x)'] = (df_2['F(x)']*0.4/5.6)/0.678
+    df_2['Cumulative V(x)'] = np.cumsum(df_2['V(x)'])
+
+    P50_2 = float(df_1.iloc[(np.cumsum(df_2['F(x)'])-0.5).abs().argsort()[:1]]['x'])
+    P80_2 = float(df_1.iloc[(np.cumsum(df_2['F(x)'])-0.8).abs().argsort()[:1]]['x'])
+
+    F_80 = np.linspace(P80_2,2000,500)
+    P_80 = P80_2
+
+    W_i = 15
+    E_G = 10*W_i*((1/np.sqrt(P_80)-(1/np.sqrt(F_80))))
+
+    plt.plot(F_80,E_G,label=r'$15$ (kWh/t)')
+
+    W_i = int(wi_var.get())
+    E_G = 10*W_i*((1/np.sqrt(P_80)-(1/np.sqrt(F_80))))
+
+    plt.plot(F_80,E_G,label=str(wi_var.get()) + '(kWh/t)')
+
+    W_i = 25
+    E_G = 10*W_i*((1/np.sqrt(P_80)-(1/np.sqrt(F_80))))
+
+    plt.plot(F_80,E_G,label=r'$25$ (kWh/t)')
+    plt.title(r'$E_G$ vs $P_{80}$ (Ball Mill, $P_{80}$ $\mu$m)')
+
+    plt.legend(title=r'$W_i$')
+    plt.xlabel(r'$F_{80}$ (microns)') 
+    plt.ylabel(r'$E_G$ (kWh/t)')
+    plt.savefig('2.jpg',dpi=500)
+    
+    image = Image.open(cd + "/2.jpg")
+    image.resize(300,200)
+    
+    g2 = ImageTk.PhotoImage(Image.open(image))
+    graph2_label = Label(foreground, image=g2).place(x=1100, y=600)
+
+
+#create widgets
+
+#creates labels in foreground
+headframe_label = Label(foreground, text = "Mission Mine Feed")
+crusher_label = Label(foreground, text = "Primary Crusher")
+SAG1_label = Label(foreground, text = "SAG Mill #1")
+SAG2_label = Label(foreground, text = "SAG Mill #2")
+Cyclone1_label = Label(foreground, text = "Hydrocyclone #1")
+Cyclone2_label = Label(foreground, text = "Hydrocyclone #2")
+Ball_label = Label(foreground, text = "Ball Mill")
+Cir1_label = Label(foreground, text = "Circulating Load")
+Cir2_label = Label(foreground, text = "Circulating Load")
+Float_label = Label(foreground, text = "Flotation Systems")
+Thick_label = Label(foreground, text = "Concentrate Thickening")
+Filt_label = Label(foreground, text = "Concentrate Pressure Filtration")
+Conc_label = Label(foreground, text = "Copper Concentrate")
+ASARCO_label = Label(foreground, text = "To ASARCO Smelter")
+Hayden_label = Label(foreground, text = "in Hayden, AZ")
+
+feed_label = Label(foreground, text= "Feed Rate (t/hr):")
+wi_label = Label(foreground, text="Bond's Work Index:")
+
+Dis1_label = Label(foreground, text= "")
+Dis2_label = Label(foreground, text= "")
+Dis3_label = Label(foreground, text= "")
+OF1_label = Label(foreground, text= "")
+OF2_label = Label(foreground, text= "")
+
+ratio1_label = Label(foreground, text= "O/F U/F Ratio:")
+ratio2_label = Label(foreground, text= "O/F U/F Ratio:")
+
+
+#creates text variables for spin boxes in foreground
+feed_var = StringVar(value=0)
+wi_var = StringVar(value=0)
+OF1_var = StringVar(value=0)
+OF2_var = StringVar(value=0)
+
+#creates spin boxes in foreground
+feed_box = Spinbox(foreground,from_=0, to=10000, textvariable=feed_var, command=update)
+wi_box = Spinbox(foreground,from_=0, to=10000, textvariable=wi_var, command=update)
+OF1_box = Spinbox(foreground,from_=0, to=1, textvariable=OF1_var, increment=0.1, width=5,  command=update)
+OF2_box = Spinbox(foreground,from_=0, to=1, textvariable=OF2_var, increment=0.1, width=5, command=update)
+
+#place labels in foreground
+headframe_label.place(x=40 , y=115)
+crusher_label.place(x=70 , y=320)
+SAG1_label.place(x=285 , y=435)
+SAG2_label.place(x=285 , y=640)
+Cyclone1_label.place(x=520 , y=700)
+Cyclone2_label.place(x=940 , y=600)
+Ball_label.place(x=730 , y=435)
+Cir1_label.place(x=150 , y=790)
+Cir2_label.place(x=680 , y=600)
+Float_label.place(x=940 , y=100)
+Thick_label.place(x=620 ,y=85)
+Filt_label.place(x=620 ,y=140)
+Conc_label.place(x=540 ,y=295)
+ASARCO_label.place(x=320, y=220)
+Hayden_label.place(x=330 ,y=240)
+
+feed_label.place(x=175, y=30)
+wi_label.place(x=175, y=80)
+
+ratio1_label.place(x=480, y=525)
+ratio2_label.place(x=880, y=437)
+
+#places spin boxes in foreground
+feed_box.place(x=175, y=50)
+wi_box.place(x=175, y=100)
+OF1_box.place(x=480 ,y=545 )
+OF2_box.place(x=880 ,y=457 )
+
+
+#creates labels in taskbar 
+Cycl1_label = Label(Taskbar, text="Select Distribution for Cyclone #1")
+Cycl2_label = Label(Taskbar, text="Select Distribution for Cyclone #2")
+
+#places labels
+Cycl1_label.place(x=1100, y=30)
+Cycl2_label.place(x=1100, y=60)
+
+#creates buttons
+Cyc1_but = Button(Taskbar, text="Browse", command=Cycl1)
+Cyc2_but = Button(Taskbar, text="Browse", command=Cycl2)
+
+#places buttons
+Cyc1_but.place(x=1330, y=30)
+Cyc2_but.place(x=1330, y=60)
+
+#establishes the mainloop of the tkinter root
+root.mainloop()
---
+++ Mission Mine/SAG.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec 19 19:21:30 2021
+
+@author: daveedo
+"""
+
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec 18 20:15:01 2021
+@author: Henry Manelski
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+#import pandas as pd
+
+# plotting for the SAG Mills 
+
+F_80 = 150000
+P_80 = np.linspace(10000,500,500)
+
+W_i = 15
+E_G = 10*W_i*((1/np.sqrt(P_80)-(1/np.sqrt(F_80))))
+
+plt.plot(P_80,E_G,label=r'$15$ (kWh/t)')
+
+W_i = 20
+E_G = 10*W_i*((1/np.sqrt(P_80)-(1/np.sqrt(F_80))))
+
+plt.plot(P_80,E_G,label=r'$20$ (kWh/t)')
+
+W_i = 25
+E_G = 10*W_i*((1/np.sqrt(P_80)-(1/np.sqrt(F_80))))
+
+plt.plot(P_80,E_G,label=r'$25$ (kWh/t)')
+plt.title(r'$E_G$ vs $P_{80}$ (SAG Mill, $F_{80}=1.5$ cm)')
+
+plt.legend(title=r'$W_i$')
+plt.xlabel(r'$P_{80}$ (microns)') 
+plt.ylabel(r'$E_G$ (kWh/t)')
+plt.savefig('1.jpg',dpi=500)
+plt.show()
+
+
+
+# plotting for the Ball Mills
+
+F_80 = np.linspace(50,2000,500)
+P_80 = 10
+
+W_i = 15
+E_G = 10*W_i*((1/np.sqrt(P_80)-(1/np.sqrt(F_80))))
+
+plt.plot(F_80,E_G,label=r'$15$ (kWh/t)')
+
+W_i = 20
+E_G = 10*W_i*((1/np.sqrt(P_80)-(1/np.sqrt(F_80))))
+
+plt.plot(F_80,E_G,label=r'$20$ (kWh/t)')
+
+W_i = 25
+E_G = 10*W_i*((1/np.sqrt(P_80)-(1/np.sqrt(F_80))))
+
+plt.plot(F_80,E_G,label=r'$25$ (kWh/t)')
+plt.title(r'$E_G$ vs $P_{80}$ (Ball Mill, $P_{80}=10$ $\mu$m)')
+
+plt.legend(title=r'$W_i$')
+plt.xlabel(r'$F_{80}$ (microns)') 
+plt.ylabel(r'$E_G$ (kWh/t)')
+plt.savefig('2.jpg',dpi=500)
+plt.show()
